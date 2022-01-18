df36af828a698906b5f50241c26173693c6342f3
Sonoraaron <97190684+Sonoraaron@users.noreply.github.com>
2022-01-18 16:33:16 -0700

    Add files via upload

--- AaronsWebPage.js
+++ AaronsWebPage.js
@@ -2,44 +2,81 @@
 <html>
 
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<!--------------------------------Title------------------------------------------>
 
 	<title>Aaron's Web Page</title>
+	<!--------------------------------Title------------------------------------------>
 
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" />
-
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
 	<link href="style.css" rel="stylesheet" type="text/css" />
-
 </head>
 
-<body>
-	<h2>An Intro section that contains the following information:</h2>
+<main>
 
-	<ul>
-		<li>Who you are</li>
+	<header>
+		<h1>Aaron Loera</h1>
+	</header>
 
-		<li>Where you are located</li>
-	</ul>
+	<nav></nav>
 
-	<h2>An About Me section that contains the following information:</h2>
+	<body>
 
-	<ul>
-		<li>A description that shows your passion for web development and your willingness to work with others</li>
+		<h2>An Intro section that contains the following information:</h2>
+		<section>
+			<ul>
+				<li>Who you are</li>
 
-		<li>One or two sentences that describe other interests you have outside of web development</li>
-	</ul>
+				<p>
+					<!--So, who are you?-->
+				</p>
+				<li>Where you are located</li>
+				<p>
+					<!--So, where are you?-->
+				</p>
+			</ul>
+		</section>
 
-	<h2>A Contact section that contains the following information:</h2>
+		<section>
+			<h2>An About Me section that contains the following information:</h2>
 
-	<ul>
-		<li>A link to your LinkedIn profile</li>
+			<ul>
+				<li>A description that shows your passion for web development and your willingness to work with others</li>
 
-		<li>A link to your GitHub profile</li>
+				<p>
+					<!--Well, get to it!-->
+				</p>
 
-		<li>A link to your email (optional)</li>
-	</ul>
+				<li>One or two sentences that describe other interests you have outside of web development</li>
 
-	<script src="script.js">
-	</script>
-</body>
+				<p>
+					<!--Fortunately, 1 or 2 sentences will be nough-->
+				</p>
+
+			</ul>
+		</section>
+
+		<!------------Contact Details------------>
+		<section>
+			<h2>A Contact section that contains the following information:</h2>
+
+			<ul>
+				<li>A link to your LinkedIn profile</li>
+
+				<li>A link to your GitHub profile</li>
+
+				<li>A link to your email (optional)</li>
+			</ul>
+		</section>
+    <!----------End Contact Details---------->
+
+		<script src="script.js">
+
+		</script>
+
+	</body>
+
+</main>
 
 </html>
