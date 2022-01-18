deeb9be802b8dadfa68f77910fac494671dea806
Sonoraaron <97190684+Sonoraaron@users.noreply.github.com>
2022-01-18 14:09:02 -0700

    Add files via upload

---
+++ AaronsWebPage.js
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<title>Aaron's Web Page</title>
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" />
+
+	<link href="style.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+	<h2>An Intro section that contains the following information:</h2>
+
+	<ul>
+		<li>Who you are</li>
+
+		<li>Where you are located</li>
+	</ul>
+
+	<h2>An About Me section that contains the following information:</h2>
+
+	<ul>
+		<li>A description that shows your passion for web development and your willingness to work with others</li>
+
+		<li>One or two sentences that describe other interests you have outside of web development</li>
+	</ul>
+
+	<h2>A Contact section that contains the following information:</h2>
+
+	<ul>
+		<li>A link to your LinkedIn profile</li>
+
+		<li>A link to your GitHub profile</li>
+
+		<li>A link to your email (optional)</li>
+	</ul>
+
+	<script src="script.js">
+	</script>
+</body>
+
+</html>
